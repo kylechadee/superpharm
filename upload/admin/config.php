e3cde2b32cd96e21c47a24c96c38551da53d3f49
Mickel McMillan <mickelmcmillan@gmail.com>
2017-03-06 14:29:02 -0400

    Add files via upload

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/SuperPharm/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/SuperPharm/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/SuperPharm/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/SuperPharm/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/SuperPharm/upload/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/SuperPharm/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/SuperPharm/upload/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/SuperPharm/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/SuperPharm/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/SuperPharm/upload/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/SuperPharm/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'superpharm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
