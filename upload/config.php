98aee04a87bae8a48aa836ad719a8607dfbeca3a
mikael.montoute <mikael.montoute@my.uwi.edu>
2017-03-11 10:39:32 -0400

    testing share ability

--- upload/config.php
+++ upload/config.php
@@ -1,22 +1,21 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8080/SuperPharm/upload/');
+define('HTTP_SERVER', 'http://localhost:8080/superpharm/upload/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8080/SuperPharm/upload/');
+define('HTTPS_SERVER', 'http://localhost:8080/superpharm/upload/');
 
-// DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/SuperPharm/upload/catalog/');
-define('DIR_SYSTEM', 'C:/xampp/htdocs/SuperPharm/upload/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/SuperPharm/upload/image/');
-define('DIR_LANGUAGE', 'C:/xampp/htdocs/SuperPharm/upload/catalog/language/');
-define('DIR_TEMPLATE', 'C:/xampp/htdocs/SuperPharm/upload/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/xampp/htdocs/SuperPharm/upload/system/config/');
-define('DIR_CACHE', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/download/');
-define('DIR_LOGS', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/xampp/htdocs/SuperPharm/upload/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/superpharm/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/superpharm/upload/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/superpharm/upload/image/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
